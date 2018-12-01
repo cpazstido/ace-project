91f2b0b0731b07c95ebf4c7f42bc8b927215a0ed
cpazstido <120964417@qq.com>
2018-12-01 23:35:30 +0800

    go on

---
+++ trade-system/trade-common/src/main/java/com/ace/trade/common/constants/MQEnums.java
@@ -0,0 +1,25 @@
+package com.ace.trade.common.constants;
+
+public class MQEnums {
+    public enum TopicEnum{
+        ORDER_CONFIRM("orderTopic","confirm"),
+        ORDER_CANCEL("orderTopic","cancel"),
+        PAY_PAID("orderTopic","paid"),
+        ;
+        private String topic;
+        private String tag;
+
+        public String getTopic() {
+            return topic;
+        }
+
+        public String getTag() {
+            return tag;
+        }
+
+        TopicEnum(String topic, String tag) {
+            this.topic = topic;
+            this.tag = tag;
+        }
+    }
+}
