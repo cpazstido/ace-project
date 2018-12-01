8bb535f84833f54608ba4268490fc88f1b98e2a9
cpazstido <120964417@qq.com>
2018-12-01 15:09:07 +0800

    go on

--- trade-system/trade-common/src/main/java/com/ace/trade/common/constants/TradeEnum.java
+++ trade-system/trade-common/src/main/java/com/ace/trade/common/constants/TradeEnum.java
@@ -1,9 +1,43 @@
 package com.ace.trade.common.constants;
 
 public class TradeEnum {
-    public enum RetEnum{
-        SUCCESS("1","成功"),
-        FAIL("-1","失败");
+    public enum RestServerEnum {
+        ORDER("localhost","order",8080),
+        PAY("localhost","pay",8081),
+        COUPON("localhost","coupon",8082),
+        GOODS("localhost","goods",8083),
+        USER("localhost","user",8084)
+        ;
+        private String serverHost;
+        private int serverPort;
+        private String contextPath;
+
+        public String getServerHost() {
+            return serverHost;
+        }
+
+        public int getServerPort() {
+            return serverPort;
+        }
+
+        public String getContextPath() {
+            return contextPath;
+        }
+
+        public String getServerUrl(){
+            return "http://"+this.serverHost+":"+this.serverPort+"/"+this.contextPath+"/";
+        }
+
+        RestServerEnum(String serverHost, String contextPath, int serverPort) {
+            this.serverHost = serverHost;
+            this.serverPort = serverPort;
+            this.contextPath = contextPath;
+        }
+    }
+
+    public enum RetEnum {
+        SUCCESS("1", "成功"),
+        FAIL("-1", "失败");
         private String code;
         private String desc;
 
--- trade-system/trade-user/src/main/java/com/ace/trade/user/api/UserApiImpl.java
+++ trade-system/trade-user/src/main/java/com/ace/trade/user/api/UserApiImpl.java
@@ -6,10 +6,7 @@ import com.ace.trade.common.protocol.user.QueryUserRes;
 import com.ace.trade.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserApiImpl implements IUserApi {
@@ -18,7 +15,7 @@ public class UserApiImpl implements IUserApi {
 
     @RequestMapping(value = "/queryUserById",method = RequestMethod.POST)
     @ResponseBody
-    public QueryUserRes queryUserById(@RequestParam QueryUserReq queryUserReq) {
+    public QueryUserRes queryUserById(@RequestBody QueryUserReq queryUserReq) {
         return this.userService.queryUserById(queryUserReq);
     }
 }
